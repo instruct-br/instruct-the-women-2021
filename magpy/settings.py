6b7a338a1dd0bf208354f93ef4ab56b98c4774c5
Miguel Di Ciurcio Filho <miguel@instruct.com.br>
2021-10-19 18:04:55 -0300

    feat: melhoras em DX (#2)
    * feat: cria usuario para API
    
    * feat: adiciona gitlens
    
    * feat: melhora ordem e explicações sobre o ambiente
    
    * corrige typo
    
    * chore: remove auth
    
    Remove botão desnecessário de autorização
    da UI da API
    
    Co-authored-by: Samylla Rodrigues <samylla.rodrigues@instruct.com.br>

--- magpy/settings.py
+++ magpy/settings.py
@@ -133,6 +133,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "EXCEPTION_HANDLER": "api.views.magpy_exception_handler",
+    "DEFAULT_AUTHENTICATION_CLASSES": []
 }
 
 SPECTACULAR_SETTINGS = {
